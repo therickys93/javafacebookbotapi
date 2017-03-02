7719c68f8684f4251f27f0c04ff8960638a4b4f9
Riccardo Crippa <therickys93@gmail.com>
2017-03-02 16:50:26 +0100

    libreria funziona correttamente

--- src/test/java/it/therickys93/facebookbot/SendMessageTest.java
+++ src/test/java/it/therickys93/facebookbot/SendMessageTest.java
@@ -2,8 +2,6 @@ package it.therickys93.facebookbot;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
-
 import org.junit.Test;
 
 public class SendMessageTest {
@@ -22,12 +20,5 @@ public class SendMessageTest {
 		assertEquals("BOT_TOKEN", bot.token());
 		assertEquals("https://graph.facebook.com/v2.6/me/messages?access_token=", FacebookBot.API_URL);
 		assertEquals("https://graph.facebook.com/v2.6/me/messages?access_token=BOT_TOKEN", bot.endpoint);
-	}
-	
-	public void testThree() throws IOException {
-		FacebookBot bot = new FacebookBot("TOKEN");
-		String response = bot.execute(new SendMessage("USER_ID", "message"));
-		assertEquals("", response);
-	}
-	
+	}	
 }
